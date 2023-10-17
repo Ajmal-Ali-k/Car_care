a00389f2481574d739a62ce2090c76f41da79d8c
Ajmal-Ali-k <ajmalali749@gmail.com>
2023-10-18 00:43:53 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+import Image from "next/image"
+
+import { CustomButton } from "."
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
