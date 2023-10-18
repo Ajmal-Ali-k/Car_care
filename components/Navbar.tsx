38e775852063506761f2a3574fd0009cda026408
Ajmal-Ali-k <ajmalali749@gmail.com>
2023-10-18 14:47:56 +0530

    navbar completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,32 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
-import { CustomButton } from "."
+import CustomButton from "./CustomButton";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="w-full absolute z-10 ">
+      <nav
+        className="max-w-[1440px] mx-auto flex justify-between items-center 
+       sm:px-16 px-6 py-4 bg-transparent"
+      >
+        <Link href="/" className="flex justify-center item-center">
+          <Image
+            src="/logo.svg"
+            alt="nav-logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+        <CustomButton
+          title="Sign in"
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
